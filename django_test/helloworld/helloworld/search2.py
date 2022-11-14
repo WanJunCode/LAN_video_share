be8910fb27ea7355d125e7efcc768c8c3a1b4a4c
wan_jun <wanjuncode@163.com>
2022-11-14 20:54:12 +0800

    完成 python web端发送 json 数据

--- django_test/helloworld/helloworld/search2.py
+++ django_test/helloworld/helloworld/search2.py
@@ -1,22 +1,40 @@
 # -*- coding: utf-8 -*-
- 
+
 from django.shortcuts import render
 from django.views.decorators import csrf
 import socket
 import time
+import json
+import random
 
 host = '127.0.0.1'
 port = 123
 MaxBytes = 1024*1024
+sequence = random.randint(1, 1024)
 
-def GetResponse(req):
+
+def ConvertToJson(json_request):
+    global sequence  # https://blog.csdn.net/weixin_45666566
+    sequence += 1
+    json_request["sequence"] = sequence
+    json_str = json.dumps(json_request)
+    print(json_str)
+    return json_str
+
+
+def GetResponse(json_request):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.settimeout(30)
     try:
         client.connect((host, port))
     except:
         return 'failed to connect'
-    sendBytes = client.send(req.encode())
+
+    # json 数据发送
+    json_request_str = ConvertToJson(json_request)
+    print("json request ", json_request_str)
+
+    sendBytes = client.send(json_request_str.encode())
     if sendBytes <= 0:
         return 'failed to send'
     recvData = client.recv(MaxBytes)
@@ -26,10 +44,15 @@ def GetResponse(req):
     return bytes.decode(recvData)
 
 # 接收POST请求数据
+
+
 def search_post(request):
-    ctx ={}
+    ctx = {}
     if request.POST:
-        ctx['rlt'] = request.POST['q']
+        ctx['rlt'] = {
+            "name": request.POST['name'],
+            "value": request.POST['value']
+        }
         # 客户端发送 request.POST['q'] 并接收C++服务端响应
-        ctx['response'] = GetResponse(request.POST['q'])
+        ctx['response'] = GetResponse(ctx['rlt'])
     return render(request, "post.html", ctx)
